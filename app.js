6fc97be694c76eed5bf2bfafd3d3f4117e3ca491
lfeliperz <feliperodriguez1006@gmail.com>
2023-01-30 16:55:54 -0500

    Arreglos

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+
 let userChoice
 let computerChoice
 let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+
   userChoice = e.target.id
   userChoiceDisplay.innerHTML = userChoice
   generateComputerChoice()
   getResult()
+
 }))
 
 function generateComputerChoice() {
+
   const randomNumber = Math.floor(Math.random() * 3) + 1
   
   if (randomNumber === 1) {
@@ -27,6 +31,7 @@ function generateComputerChoice() {
   }
   computerChoiceDisplay.innerHTML = computerChoice
 }
+
 
 function getResult() {
   if (computerChoice === userChoice) {
